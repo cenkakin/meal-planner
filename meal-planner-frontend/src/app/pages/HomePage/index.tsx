623e5534efcfe44c0359f489e4210808a00f615d
Serkan Kaan Can Koyak <serkan.koyak@jimdo.com>
2023-02-23 00:37:12 +0100

    Add multiselect to ingredient selection

--- meal-planner-frontend/src/app/pages/HomePage/index.tsx
+++ meal-planner-frontend/src/app/pages/HomePage/index.tsx
@@ -1,27 +1,59 @@
 import Autocomplete from '@mui/material/Autocomplete';
 import * as React from 'react';
-import { Helmet } from 'react-helmet-async';
-import { TextField } from '@mui/material';
+import {Helmet} from 'react-helmet-async';
+import {TextField} from '@mui/material';
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {config} from "shelljs";
+
 
 const options = [
-  { label: 'Apple', id: 1 },
-  { label: 'Orange', id: 2 },
+    {label: 'Apple', id: 1},
+    {label: 'Orange', id: 2},
 ];
 
+type IngredientAutocompleteItem = {
+    id: string,
+    name: string
+}
+
 export function SearchRecipes() {
-  return (
-    <>
-      <Helmet>
-        <title>Search Recipes</title>
-        <meta name="description" content="Find your recipes" />
-      </Helmet>
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={options}
-        sx={{ width: 300 }}
-        renderInput={params => <TextField {...params} label="Ingredient" />}
-      />
-    </>
-  );
+    const [ingredients, setIngredients] = useState<Array<IngredientAutocompleteItem>>([])
+
+    async function getIngredients() {
+
+        let {data: response} = await axios.get("http://localhost:8080/v1/ingredient")
+
+        setIngredients(response.ingredients.map((ingredient) => {
+            let ingredientAutocompleteItem: IngredientAutocompleteItem = {name: ingredient.name, id: ingredient.id};
+            return ingredientAutocompleteItem
+        }))
+    }
+
+    useEffect(() => {
+        getIngredients()
+    }, [])
+
+    return (
+        <>
+            <Helmet>
+                <title>Search Recipes</title>
+                <meta name="description" content="Find your recipes"/>
+            </Helmet>
+            <Autocomplete
+                multiple
+                id="tags-outlined"
+                options={ingredients}
+                getOptionLabel={(option) => option.name}
+                filterSelectedOptions
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="filterSelectedOptions"
+                        placeholder="Ingredients"
+                    />
+                )}
+            />
+        </>
+    );
 }
