5499b15ad9e780ff34b429d5272a06a71d2284d9
Serkan Kaan Can Koyak <serkan.koyak@jimdo.com>
2023-02-23 17:46:15 +0100

    Add recipe search functionality
    Recipes found based on the selection of ingredients are
    listed in the table below the search box/button

--- meal-planner-frontend/src/app/pages/HomePage/index.tsx
+++ meal-planner-frontend/src/app/pages/HomePage/index.tsx
@@ -1,10 +1,11 @@
 import Autocomplete from '@mui/material/Autocomplete';
 import * as React from 'react';
 import {Helmet} from 'react-helmet-async';
-import {TextField} from '@mui/material';
-import {useEffect, useState} from "react";
+import {Button, Container, TextField} from '@mui/material';
+import {SyntheticEvent, useEffect, useState} from "react";
 import axios from "axios";
-import {config} from "shelljs";
+import BasicRecipeTable from "../../common/components/BasicRecipeTable";
+import Grid2 from "@mui/material/Unstable_Grid2";
 
 
 const options = [
@@ -12,48 +13,117 @@ const options = [
     {label: 'Orange', id: 2},
 ];
 
-type IngredientAutocompleteItem = {
+interface IngredientAutocompleteItem {
     id: string,
     name: string
 }
 
+interface BasicRecipeItem {
+    id: string,
+    name: string,
+    cuisine: string
+}
+
 export function SearchRecipes() {
-    const [ingredients, setIngredients] = useState<Array<IngredientAutocompleteItem>>([])
-
-    async function getIngredients() {
-
-        let {data: response} = await axios.get("http://localhost:8080/v1/ingredient")
-
-        setIngredients(response.ingredients.map((ingredient) => {
-            let ingredientAutocompleteItem: IngredientAutocompleteItem = {name: ingredient.name, id: ingredient.id};
-            return ingredientAutocompleteItem
-        }))
-    }
+    const [ingredients, setIngredients] = useState<readonly IngredientAutocompleteItem[]>([])
+    const [basicRecipes, setBasicRecipes] = useState<readonly BasicRecipeItem[]>([])
+    const [ingredientsSelection, setIngredientsSelection] = useState<IngredientAutocompleteItem[]>([])
+    const [open, setOpen] = React.useState(false);
+    const loading = open && ingredients.length === 0;
 
     useEffect(() => {
-        getIngredients()
-    }, [])
+        let active = true;
+
+        if (!loading) {
+            return undefined;
+        }
+
+        (async () => {
+            let {data: response} = await axios.get("http://localhost:8080/v1/ingredient")
+
+            if (active) {
+                setIngredients(response.ingredients.map((ingredient) => {
+                    let ingredientAutocompleteItem: IngredientAutocompleteItem = {
+                        name: ingredient.name,
+                        id: ingredient.id
+                    };
+                    return ingredientAutocompleteItem
+                }))
+            }
+        })();
+
+        return () => {
+            active = false;
+        };
+    }, [loading]);
+
+
+    function handleSelection(e: SyntheticEvent, newValue: IngredientAutocompleteItem[]) {
+        setIngredientsSelection(newValue)
+    }
+
+    async function handleSearchRecipe(){
+        await axios.get("http://localhost:8080/v1/recipe/search?ingredientIds="+ingredientsSelection.map(i => i.id)).then(
+            response => (
+                setBasicRecipes(response.data.recipes.map((basicRecipe) => {
+                    let basicRecipeItem: BasicRecipeItem = {
+                        name: basicRecipe.name,
+                        id: basicRecipe.id,
+                        cuisine: basicRecipe.cuisine
+                    }
+                    return basicRecipeItem
+                }))
+            )
+        ).catch((e) => {throw (e)})
+    }
 
     return (
-        <>
+        <Grid2 container direction={"column"}
+            style={{paddingTop: 20}}>
             <Helmet>
                 <title>Search Recipes</title>
                 <meta name="description" content="Find your recipes"/>
             </Helmet>
-            <Autocomplete
-                multiple
-                id="tags-outlined"
-                options={ingredients}
-                getOptionLabel={(option) => option.name}
-                filterSelectedOptions
-                renderInput={(params) => (
-                    <TextField
-                        {...params}
-                        label="filterSelectedOptions"
-                        placeholder="Ingredients"
+            <Grid2 container direction={"row"}>
+                <Grid2>
+                    <Autocomplete
+                        multiple
+                        limitTags={4}
+                        id="tags-outlined"
+                        open={open}
+                        onOpen={() => {
+                            setOpen(true);
+                        }}
+                        onClose={() => {
+                            setOpen(false);
+                        }}
+                        value={ingredientsSelection}
+                        onChange={handleSelection}
+                        options={ingredients}
+                        getOptionLabel={(option) => option.name}
+                        filterSelectedOptions
+                        loading={loading}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Select ingredients"
+                                placeholder="Ingredients"
+                            />
+                        )}
+                        sx={{width: '500px'}}
                     />
-                )}
-            />
-        </>
+                </Grid2>
+                <Grid2>
+                    <Button
+                        variant="contained"
+                        onClick={handleSearchRecipe}
+                    >Search</Button>
+                </Grid2>
+            </Grid2>
+            <Grid2>
+                {basicRecipes.length > 0 && <BasicRecipeTable inputData={basicRecipes}/>}
+            </Grid2>
+        </Grid2>
+
     );
 }
