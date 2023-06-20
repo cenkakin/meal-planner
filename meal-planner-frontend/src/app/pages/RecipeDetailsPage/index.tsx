b790890e9e85c01416742fcdae9368a30da84c9f
Serkan Kaan Can Koyak <serkan.koyak@jimdo.com>
2023-06-20 18:07:28 +0200

    Display images, name, calories, ingredients, instructions of the recipe
    in Recipe Details page

--- meal-planner-frontend/src/app/pages/RecipeDetailsPage/index.tsx
+++ meal-planner-frontend/src/app/pages/RecipeDetailsPage/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Grid2 from '@mui/material/Unstable_Grid2';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 import { useEffect, useState } from 'react';
 import httpClient from '../../common/http-common';
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { Recipe } from './RecipeDetailsView/Recipe';
 
 export function RecipeDetails() {
@@ -18,11 +20,13 @@ export function RecipeDetails() {
         name: response.name,
         cuisine: response.cuisine,
         recipeIngredients: response.recipeIngredients,
+        instructions: response.instructions,
+        recipeImages: response.recipeImages,
       };
       setRecipe(recipeItem);
     };
     fetchRecipe();
-  }, []);
+  }, [recipeId]);
 
   return (
     <Grid2
@@ -32,19 +36,54 @@ export function RecipeDetails() {
       alignItems="center"
       marginTop={10}
       spacing={5}
+      xs={10}
     >
       <Helmet>
-        <title>Recipe Details</title>
+        <title>Recipe Details </title>
         <meta name="description" content="Discover recipes" />
       </Helmet>
       <Grid2 xs={8}>
-        {recipe?.name}
-        {recipe?.recipeIngredients.map(ingredient => (
-          <li key={ingredient.ingredient.id}>
-            {ingredient.ingredient.name} - {ingredient.quantity} -{' '}
-            {ingredient.ingredient.unit}
-          </li>
-        ))}
+        <h2>
+          {recipe?.name}:{' '}
+          {recipe?.recipeIngredients
+            .map(
+              ingredient =>
+                (ingredient.weightInGram * ingredient.ingredient.energy) / 100,
+            )
+            .reduce((prelim, a) => prelim + a, 0).toFixed(0)} Calories
+        </h2>
+        <ImageList variant={'quilted'} cols={5} gap={8}>
+          {recipe != null}  {recipe?.recipeImages.map(url => (
+            <ImageListItem>
+              <img
+                src={url}
+                alt={""}
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Grid2>
+      <Grid2
+        xs={8}
+        direction={'row'}
+        container
+        justifyContent="center"
+        alignItems="flex-start"
+      >
+        <Grid2 xs={4}>
+          <h3>Ingredients:</h3>
+          {recipe?.recipeIngredients.map(ingredient => (
+            <li key={ingredient.ingredient.id}>
+              {ingredient.ingredient.name} ({ingredient.quantity} {ingredient.unit})
+            </li>
+          ))}
+        </Grid2>
+        <Grid2 xs={8}>
+          <h3>Instructions:</h3>
+          {recipe?.instructions.map(step => (
+            <li>{step}</li>
+          ))}
+        </Grid2>
       </Grid2>
     </Grid2>
   );
