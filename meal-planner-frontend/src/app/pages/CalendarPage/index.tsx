40335236aa25db06ab4e97cf0726a23aad2c21c2
Serkan Kaan Can Koyak <serkan.koyak@jimdo.com>
2023-07-15 19:40:31 +0200

    Add table instead of rendering the whole content in a list

--- meal-planner-frontend/src/app/pages/CalendarPage/index.tsx
+++ meal-planner-frontend/src/app/pages/CalendarPage/index.tsx
@@ -4,7 +4,19 @@ import Grid2 from '@mui/material/Unstable_Grid2';
 import { useEffect, useState } from 'react';
 import httpClient from '../../common/http-common';
 import { CalendarEntry } from './CalendarView/CalendarEntry';
-import { Divider, List, ListItem, ListItemText } from '@mui/material';
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import Paper from '@mui/material/Paper';
 
 export function Calendar(props) {
   const [calendar, setCalendar] = useState<[CalendarEntry]>();
@@ -49,47 +61,78 @@ export function Calendar(props) {
     fetchCalendar();
   }, [calendarId]);
 
+  function calculateTotalCalories(recipes) {
+    return (
+      recipes
+        .map(
+          recipe =>
+            recipe.recipeIngredients
+              .map(
+                ingredient =>
+                  (ingredient.weightInGram * ingredient.ingredient.energy) /
+                  100,
+              )
+              .reduce((prelim, a) => prelim + a, 0),
+          // .toFixed(0)
+        )
+        .reduce((prelim, a) => prelim + a, 0)
+        .toFixed(0) + ' Cal'
+    );
+  }
+
+  function createMealList(entries) {
+    return (
+      <List>
+        {entries.map(recipe => (
+          <ListItem>
+            <ListItemText
+              primary={recipe.name}
+              secondary={calculateTotalCalories([recipe])}
+            />
+          </ListItem>
+        ))}
+      </List>
+    );
+  }
+
   return (
     <Grid2 direction={'column'} marginTop={10} spacing={5}>
       <Helmet>
         <title>{props.title}</title>
         <meta name="description" content="Discover recipes" />
       </Helmet>
-      <List sx={{ width: '100%', maxWidth: 760, bgcolor: 'background.paper' }}>
-        {calendar?.map(entry =>
-          entry.entries.map(recipe => (
-            <ListItem alignItems="center" sx={{ justifyContent: 'flex-start' }}>
-              <ListItemText
-                primary={entry.date}
-                sx={{
-                  width: '100%',
-                  maxWidth: 160,
-                  bgcolor: 'background.paper',
-                }}
-              />
-              <ListItemText
-                primary={recipe.name}
-                secondary={
-                  recipe.recipeIngredients
-                    .map(
-                      ingredient =>
-                        (ingredient.weightInGram *
-                          ingredient.ingredient.energy) /
-                        100,
-                    )
-                    .reduce((prelim, a) => prelim + a, 0)
-                    .toFixed(0) + ' Cal'
-                }
-                sx={{
-                  width: '100%',
-                  maxWidth: 260,
-                  bgcolor: 'background.paper',
-                }}
-              />
-            </ListItem>
-          )),
-        )}
-      </List>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell width="25%" align="left">
+                Date
+              </TableCell>
+              <TableCell width="50%" align="left">
+                Meals
+              </TableCell>
+              <TableCell width="25%" align="left">
+                Total Calories
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {calendar?.map(entry => (
+              <TableRow>
+                <TableCell width="25%" align="left">
+                  {entry.date}
+                </TableCell>
+                <TableCell width="50%" align="left">
+                  {createMealList(entry.entries)}
+                </TableCell>
+                <TableCell width="25%" align="left">
+                  {calculateTotalCalories(entry.entries)}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Grid2>
   );
 }
