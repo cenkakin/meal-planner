ba1f09c8dce4f733ae4fd86f727d5d5782863247
Serkan Kaan Can Koyak <serkan.koyak@jimdo.com>
2023-07-21 21:37:47 +0200

    Add migration: Create User & Calendar tables

---
+++ src/main/resources/db/migration/V02__create_tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE "user"
+(
+    id         UUID      NOT NULL DEFAULT gen_random_uuid(),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    user_name  VARCHAR   NOT NULL UNIQUE,
+    password   VARCHAR   NOT NULL,
+    email      VARCHAR   NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+CREATE INDEX email_index ON "user" ("id");
+
+CREATE TABLE calendar
+(
+    id          UUID      NOT NULL DEFAULT gen_random_uuid(),
+    created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at  TIMESTAMP NOT NULL DEFAULT NOW(),
+    user_id     UUID      NOT NULL,
+    "date"      DATE,
+    "recipe_id" UUID,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_user_id
+        FOREIGN KEY (user_id)
+            REFERENCES "user" (id)
+);
+CREATE INDEX date_index ON calendar("date")
