a91f90b5832dd1b6ff31920316db70c61e977fc0
Serkan Kaan Can Koyak <serkan.koyak@jimdo.com>
2023-07-23 03:46:26 +0200

    Move controllers to separate package

--- src/main/kotlin/com/github/cenkserkan/infra/adapters/recipe/rest/controller/CalendarController.kt
+++ src/main/kotlin/com/github/cenkserkan/infra/adapters/recipe/rest/controller/CalendarController.kt
@@ -1,4 +1,4 @@
-package com.github.cenkserkan.infra.adapters.recipe.rest
+package com.github.cenkserkan.infra.adapters.recipe.rest.controller
 
 import com.github.cenkserkan.domain.calendar.usecase.GetCalendarEntriesUseCase
 import com.github.cenkserkan.domain.calendar.usecase.SaveCalendarEntriesUseCase
@@ -7,6 +7,7 @@ import com.github.cenkserkan.infra.adapters.recipe.rest.dto.CalendarEntryRequest
 import com.github.cenkserkan.infra.adapters.recipe.rest.dto.CalendarEntryResponse
 import jakarta.validation.constraints.NotEmpty
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -22,6 +23,8 @@ class CalendarController(
     private val addCalendarEntryUseCase: SaveCalendarEntriesUseCase,
     private val getCalendarEntriesUseCase: GetCalendarEntriesUseCase
 ) {
+
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/{userId}")
     fun getCalendarEntries(
         @PathVariable @NotEmpty
@@ -32,6 +35,7 @@ class CalendarController(
         return ResponseEntity.ok(CalendarEntryListResponse(calendarEntries))
     }
 
+    @PreAuthorize("hasRole('USER')")
     @PostMapping
     fun addCalendarEntries(@RequestBody request: CalendarEntryRequest): ResponseEntity<String> {
         try {
