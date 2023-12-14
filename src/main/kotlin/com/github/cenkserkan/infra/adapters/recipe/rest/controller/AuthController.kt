9262439e940ae015a1b2e97490d7bc926e8a4851
Serkan Kaan Can Koyak <serkan.koyak@jimdo.com>
2023-12-14 02:58:53 +0100

    Refactor AuthController

--- src/main/kotlin/com/github/cenkserkan/infra/adapters/recipe/rest/controller/AuthController.kt
+++ src/main/kotlin/com/github/cenkserkan/infra/adapters/recipe/rest/controller/AuthController.kt
@@ -1,10 +1,8 @@
 package com.github.cenkserkan.infra.adapters.recipe.rest.controller
 
-import app.domain.handler.AuthHandler
+import com.github.cenkserkan.auth.AuthUsecase
 import com.github.cenkserkan.infra.adapters.recipe.rest.dto.LoginRequest
 import com.github.cenkserkan.infra.adapters.recipe.rest.dto.RegisterRequest
-import jakarta.servlet.http.HttpServletResponse
-import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,29 +12,20 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("v1/auth")
 class AuthController(
-    private val authHandler: AuthHandler
+    private val authUsecase: AuthUsecase
 ) {
     @PostMapping("/register")
     fun register(@RequestBody request: RegisterRequest): ResponseEntity<String> {
-        authHandler.register(request.toUser())
-
-        return ResponseEntity.ok("User registered successfully!")
+        return ResponseEntity.ok(authUsecase.register(request.toUser()))
     }
 
     @PostMapping("/login")
-    fun login(@RequestBody request: LoginRequest, response: HttpServletResponse): ResponseEntity<String> {
-        val cookie = authHandler.login(email = request.userName, password = request.password)
-
-        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).body("Login Successful!")
+    fun login(@RequestBody request: LoginRequest): ResponseEntity<String> {
+        return ResponseEntity.ok(authUsecase.login(email = request.email, password = request.password))
     }
 
     @PostMapping("/logout")
     fun logOut(): ResponseEntity<String> {
-        // val cleanCookie = authHandler.logOut()
-        //
-        // return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cleanCookie.toString())
-        //     .body("You've been signed out!")
-
         TODO("NOT IMPLEMENTED")
     }
 }
